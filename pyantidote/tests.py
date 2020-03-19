2aae7c4c174f0efe49e61a757cb3a10d2baf6590
jackofspades707 <jackofspades707@hotmail.com>
2020-03-19 14:53:02 -0700

    updated tests

--- pyantidote/tests.py
+++ pyantidote/tests.py
@@ -3,14 +3,14 @@
 from antidote import DB
 
 def test_known_hash():
-    print("[T] db.is_known_hash: ", end='')
+    print("[T] db.exists: ", end='')
     known_hashes = ['781770fda3bd3236d0ab8274577dddde', '86b6c59aa48a69e16d3313d982791398', '42914d6d213a20a2684064be5c80ffa9']
     unknown_hashes = ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb', 'ccccccccccccccccccccccccccccccc']
     with DB() as db:
         for known_hash in known_hashes:
-            assert db.is_known_hash(known_hash) is True
+            assert db.exists('md5_hash', 'virus_md5_hashes', known_hash) is True
         for unknown_hash in unknown_hashes:
-            assert db.is_known_hash(unknown_hash) is False
+            assert db.exists('md5_hash', 'virus_md5_hashes', unknown_hash) is False
     print("Success")
 
 
